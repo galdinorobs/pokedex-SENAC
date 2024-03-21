969ec698ba7f9b8c02caac231abf2cafa2352a3d
Robson <jrobson.santos81@gmail.com>
2024-03-20 21:04:27 -0300

    feat: acrescenta funções nos demais pokemons

--- script.js
+++ script.js
@@ -38,3 +38,83 @@ let choque = document.querySelector('.choque')
         choque.style.display = 'block'
         choque.play()
 })
+
+let folha = document.querySelector('.folha')
+let bulbasauro = document.querySelector('.imagemB')
+    bulbasauro.addEventListener('click', () => {
+        folha.style.display = 'block'
+        folha.play()
+})
+
+let fogo = document.querySelector('.folha')
+let charmander = document.querySelector('.imagemC')
+    charmander.addEventListener('click', () => {
+        fogo.style.display = 'block'
+        fogo.play()
+})
+
+let agua = document.querySelector('.agua')
+let gyarados = document.querySelector('.imagemD')
+    gyarados.addEventListener('click', () => {
+        agua.style.display = 'block'
+        agua.play()
+})
+
+let fanta = document.querySelector('.fantasma')
+let gengar = document.querySelector('.imagemE')
+    gengar.addEventListener('click', () => {
+        fanta.style.display = 'block'
+        fanta.play()
+})
+
+let drago = document.querySelector('.dragao')
+let dragonite = document.querySelector('.imagemF')
+    dragonite.addEventListener('click', () => {
+        drago.style.display = 'block'
+        drago.play()
+})
+
+let aviso = document.querySelector('.aviso')
+
+function mouseEntrar() {
+    pikachu.style.transform = 'scale(1.2)'
+    bulbasauro.style.transform = 'scale(1.3)'
+    charmander.style.transform = 'scale(1.1)'
+    gyarados.style.transform = 'scale(1.1)'
+    gengar.style.transform = 'scale(1.2)'
+    dragonite.style.transform = 'scale(1.1)'
+    logo.style.transform = 'scale(1.2)'
+    //alterando a opacidade do aviso de 0 para 1
+    aviso.style.opacity = '1'
+    //alterando a escala do aviso
+    aviso.style.transform = 'scale(1)'
+}
+
+function mouseSair() {
+    pikachu.style.transform = 'scale(1)'
+    bulbasauro.style.transform = 'scale(1)'
+    charmander.style.transform = 'scale(1)'
+    gyarados.style.transform = 'scale(1)'
+    gengar.style.transform = 'scale(1)'
+    dragonite.style.transform = 'scale(1)'
+    logo.style.transform = 'scale(1.1)'
+    aviso.style.opacity = '0'
+    aviso.style.transform = 'scale(.8)'
+}
+
+listaSelecaoPokemons.forEach(pokemon => {
+
+    pokemon.addEventListener('click', () => {
+        //seleciona o elemento html que possuir a classe aberto
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        //remove a classe aberto deste pokemon para que as informações dele sjam ocultadas
+        cartaoPokemonAberto.classList.remove('aberto')
+        //pegando o nome do id do elemento html dentro de uma constante 
+        const idPokemonSelecionado = pokemon.attributes.id.value
+        //concatenando o id do pokemon com o prefixo cartao
+        const cartaoPokemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
+        //adicionando a classe aberto ao elemento card
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+    })
+})
